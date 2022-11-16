f6995f6b57590f822e6c58f32fc9ae15f92f4cd3
Bojan Zelic <bojan.zelic@qualia.com>
2022-11-15 17:02:29 -0700

    add cloudflare collections tests

--- internal/cfcollections/access_groups_test.go
+++ internal/cfcollections/access_groups_test.go
@@ -9,6 +9,98 @@ import (
 )
 
 var _ = Describe("AccessGroups", Label("AccessGroup"), func() {
+	Context("AccessGroup test", func() {
+		It("should be able able to find non-equality", func() {
+			first := cloudflare.AccessGroup{
+				Name: "test",
+				Include: []interface{}{
+					map[string]interface{}{
+						"email": map[string]interface{}{
+							"email": "good@test.com",
+						},
+					},
+				},
+			}
+
+			second := cloudflare.AccessGroup{
+				Name: "test",
+				Include: []interface{}{cloudflare.AccessGroupEmail{
+					Email: struct {
+						Email string "json:\"email\""
+					}{
+						Email: "bad@test.com",
+					},
+				}},
+			}
+
+			Expect(cfcollections.AccessGroupEqual(first, second)).To(BeFalse())
+		})
+		It("should be able able to find equality", func() {
+			first := cloudflare.AccessGroup{
+				Name: "test",
+				Include: []interface{}{
+					map[string]interface{}{
+						"email": map[string]interface{}{
+							"email": "test@test.com",
+						},
+					},
+				},
+			}
+
+			second := cloudflare.AccessGroup{
+				Name: "test",
+				Include: []interface{}{cloudflare.AccessGroupEmail{
+					Email: struct {
+						Email string "json:\"email\""
+					}{
+						Email: "test@test.com",
+					},
+				}},
+			}
+
+			Expect(cfcollections.AccessGroupEqual(first, second)).To(BeTrue())
+		})
+
+		It("should be able able to find equality with an array of elements", func() {
+			first := cloudflare.AccessGroup{
+				Name: "test",
+				Include: []interface{}{
+					map[string]interface{}{
+						"email": map[string]interface{}{
+							"email": "test@test.com",
+						},
+					},
+					map[string]interface{}{
+						"email": map[string]interface{}{
+							"email": "test2@test.com",
+						},
+					},
+				},
+			}
+
+			second := cloudflare.AccessGroup{
+				Name: "test",
+				Include: []interface{}{
+					cloudflare.AccessGroupEmail{
+						Email: struct {
+							Email string "json:\"email\""
+						}{
+							Email: "test@test.com",
+						},
+					},
+					cloudflare.AccessGroupEmail{
+						Email: struct {
+							Email string "json:\"email\""
+						}{
+							Email: "test2@test.com",
+						},
+					},
+				},
+			}
+
+			Expect(cfcollections.AccessGroupEqual(first, second)).To(BeTrue())
+		})
+	})
 	Context("AccessGroupCollection test", func() {
 		It("Should be able to find by name", func() {
 			groups := cfcollections.AccessGroupCollection{
--- internal/cfcollections/access_policy_test.go
+++ internal/cfcollections/access_policy_test.go
@@ -1,12 +1,77 @@
 package cfcollections_test
 
 import (
+	"fmt"
+
+	"github.com/bojanzelic/cloudflare-zero-trust-operator/internal/cfcollections"
+	"github.com/cloudflare/cloudflare-go"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("AccessPolicy", Label("AccessPolicy"), func() {
 	Context("AccessPolicy test", func() {
+		It("should be able to determine equality", func() {
+			first := cloudflare.AccessPolicy{
+				Name:       "test",
+				Precedence: 1,
+				Include: []interface{}{
+					map[string]interface{}{
+						"email": map[string]interface{}{
+							"email": "test@test.com",
+						},
+					},
+				},
+			}
 
+			second := cloudflare.AccessPolicy{
+				Name:       "test",
+				Precedence: 1,
+				Include: []interface{}{cloudflare.AccessGroupEmail{
+					Email: struct {
+						Email string "json:\"email\""
+					}{
+						Email: "test@test.com",
+					},
+				}},
+			}
+
+			Expect(cfcollections.AccessPoliciesEqual(&first, &second)).To(BeTrue())
+		})
+	})
+	Context("AccessPolicyCollection test", func() {
+		It("Should be able to sort by precidence", func() {
+			aps := cfcollections.AccessPolicyCollection{
+				{
+					Name:       "test4",
+					Precedence: 4,
+				},
+				{
+					Name:       "test3",
+					Precedence: 3,
+				},
+				{
+					Name:       "test2",
+					Precedence: 2,
+				},
+				{
+					Name:       "test1",
+					Precedence: 1,
+				},
+				{
+					Name:       "test5",
+					Precedence: 5,
+				},
+			}
+
+			aps.SortByPrecidence()
+
+			prevAP := cloudflare.AccessPolicy{Precedence: 0}
+			for _, ap := range aps {
+				fmt.Println(ap.Precedence)
+				Expect(ap.Precedence > prevAP.Precedence).To(BeTrue())
+				prevAP = ap
+			}
+		})
 	})
 })
